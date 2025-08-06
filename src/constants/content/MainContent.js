eb2d2de016095e5a6843f8aef5ebf49197ad0e5d
vaishali <13march.vaishali@gmail.com>
2025-08-07 03:15:25 +0530

    letest

--- src/constants/content/MainContent.js
+++ src/constants/content/MainContent.js
@@ -15,11 +15,11 @@ export const MainContent = {
 
 export const backendConfig = {
 
-  // base: "http://192.168.1.53:8080/api",
-  // origin: "http://192.168.1.53:8080",
+  base: "http://192.168.93.52:6076/api",
+  origin: "http://192.168.93.52:6076",
   //  base: "http://192.168.154.52:6077/api",  
   // origin: "http://192.168.154.52:6076",    
   //base: "https://api.zentor.us/api",
- base: "https://tradingbackend-4s5j.onrender.com/api",   
-  origin: "https://mlmdemo.bizbundle.in",  
+//  base: "https://tradingbackend-4s5j.onrender.com/api",   
+//   origin: "https://mlmdemo.bizbundle.in",  
 };
